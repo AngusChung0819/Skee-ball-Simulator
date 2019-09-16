afc9df5fb47cc01cf12867ad40d4835bbfc2ded5
AngusChung0819 <anguspro2019@gmail.com>
2019-09-16 21:25:31 +0800

    SkeeBall Force Testing

---
+++ Skee ball Game/Assets/Scripts/SkeeBall.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeeBall : MonoBehaviour {
+
+    public GameObject newBallPos;
+    public GameObject skeeball;
+
+    public float maxForce = 1550;
+    public float minForce = 950;
+    public float force = 1000;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyUp("a")) {
+            rb.AddForce(0, 0, force);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log(other.gameObject.tag);
+        if(other.gameObject.tag == "Hole") {
+            Instantiate(skeeball, newBallPos.transform.position, newBallPos.transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+
+}
